29d772c1c139546a76fef7510f9eae7f0d127b1f
Sotapulu <98513245+SotaPulu@users.noreply.github.com>
2024-05-18 01:58:40 +0300

    Fixed the bad

--- YT-autoplay.js
+++ YT-autoplay.js
@@ -4,7 +4,7 @@
 // @version      2024-05-17
 // @description  dead-simple script to bypass that annoying "Are you still watching?" popup. Is it optimized? no. Does it work? Sometimes. Did I hack it together in literally 2 minutes? absolutely.
 // @author       SotaPulu
-// @match        https://www.youtube.com/watch?v=VA69HxJW23E&list=PLNaR09R2UF4zqGS3CGLC3V_UWviSZVMY0&index=8
+// @match        https://www.youtube.com/watch?*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
